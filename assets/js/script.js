564184b8b819de988337f110f8b399931bd44736
Solomon <solomonmeresa8@gmail.com>
2020-08-22 22:01:02 -0700

    add responsivness and reminder note

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 var loadPlans = function () {
-  for (var i = 9; i <= 17; i++) {
+  for (var i = 14; i <= 22; i++) {
     // change i to start counting from 0
     var planId = parseInt(`${i - 9}`);
 
     // get data from local storage
     var dataL = localStorage.getItem(`${planId}`);
-    console.log(dataL);
 
     //create div element
-    var eventEL = $("<div>").addClass("row");
+    var eventEL = $("<div>").addClass("row d-flex");
 
     //create p element
     var eventDescription = $("<p>")
-      .addClass("description col-sm-9 p-3")
+      .addClass("description  p-3 col-sm-8 col-md-9 col-lg-9")
       .text(dataL);
     //create button element
     var eventBtn = $("<button>")
-      .addClass("saveBtn col-sm-1")
+      .addClass("saveBtn col-sm-2 col-md-1 col-lg-1")
       .html('<i class="far fa-save"></i>');
     // create dive element for time
     var eventTIme = $("<div>")
@@ -40,11 +39,22 @@ var loadPlans = function () {
     // remove any old classes from element
     $(eventEL).removeClass("list-group-item-warning list-group-item-danger");
 
-    // apply new class if task is near/over due date
+    // apply new class if task is over due time
     if (moment().isAfter(scheduleTime)) {
       $(eventDescription).addClass("past p-3");
-      // $(eventEL).addClass("list-group-item-danger");
+
+      // $(eventEL).addClass("present");
     } else if (Math.abs(moment().diff(scheduleTime, "hour")) <= 0) {
+      var ScheduleText = $(eventDescription).text();
+      if (ScheduleText) {
+        var reminder = $('<i class="far fa-bell"></i>');
+        var alert = $("<span>").text(
+          ` Get ready for " ${ScheduleText} " at - ${scheduleTime.format(
+            "hh:mm A"
+          )} `
+        );
+        $("#alert").append(reminder, alert);
+      }
       $(eventDescription).addClass("present");
     } else {
       $(eventDescription).addClass("future");
@@ -52,34 +62,36 @@ var loadPlans = function () {
   }
 
   $(".row").on("click", "p", function () {
+    // get the class name before the click happen
+    var previousCLass = $(this).closest("p").attr("class");
+
     // get current text
     planText = $(this).text().trim();
     console.log(planText);
 
-    // create new input element
-    planInput = $("<textarea>").addClass("description col-sm-9").val(planText);
+    // create new textarea element and assign it class name of the one before edit start
+    planInput = $("<textarea>").addClass(previousCLass).val(planText);
 
     $(this).replaceWith(planInput);
 
-    // automatically bring up the calendar
+    // focus for text input
     planInput.trigger("focus");
   });
 
   $(".row").on("click", "button", function () {
     // get the textarea's current value/text
     var text = $(this).siblings(".description").val().trim();
-    console.log(text);
+
+    // get the class name of 'textarea' as previous class
+    var previousCLass = $(this).siblings("textarea").attr("class");
 
     // get the parent row data-id attribute
     var index = $(this).closest(".row").attr("data-id");
-    console.log(index);
 
     //save item to localStorage
     localStorage.setItem(index, text);
-
-    var eventDescription = $("<p>")
-      .addClass("description col-sm-9 p-3")
-      .text(text);
+    // create new event element with the class name as text area
+    var eventDescription = $("<p>").addClass(previousCLass).text(text);
 
     // replace textarea with p element
     $(this).siblings(".description").replaceWith(eventDescription);
